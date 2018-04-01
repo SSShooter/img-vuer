d5865a9fcb9ee8e8f04a71eb388ccfb5fdd7122a
ssshooter <472356884@qq.com>
2018-04-01 13:34:18 +0800

    drop console in prod env

--- webpack.config.js
+++ webpack.config.js
@@ -66,7 +66,8 @@ if (process.env.NODE_ENV === 'production') {
     new webpack.optimize.UglifyJsPlugin({
       sourceMap: true,
       compress: {
-        warnings: false
+        warnings: false,
+        drop_console: true
       }
     }),
     new webpack.LoaderOptionsPlugin({
